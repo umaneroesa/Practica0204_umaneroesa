685efbb318bb7b10642de19845524359575dc3fd
umaneroesa <umaneroesa1@educacion.navarra.es>
2024-10-29 10:14:05 +0100

    Fin02

---
+++ Ejercicio02.py
@@ -0,0 +1,4 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje: Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
+Asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+for i in Asignaturas:
+    print("Yo estudio " + i)
