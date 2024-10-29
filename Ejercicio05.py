d2ab715bd5b57113387864ff09c75a58eec9929e
umaneroesa <umaneroesa1@educacion.navarra.es>
2024-10-29 10:44:53 +0100

    Fin05

---
+++ Ejercicio05.py
@@ -0,0 +1,4 @@
+#Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+lista =["1","2","3","5","6","7","8","9","10"]
+lista.reverse()
+print(lista)
