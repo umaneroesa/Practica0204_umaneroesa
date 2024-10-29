b22b516eed9b1d7ce3ee3f60f61067d0d0f364d8
umaneroesa <umaneroesa1@educacion.navarra.es>
2024-10-29 10:13:23 +0100

    Fin01

---
+++ Ejercicio01.py
@@ -0,0 +1,3 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+Asignaturas = ["Matematicas, Fisica, Quimica, Historia, Lengua"]
+print(Asignaturas)
