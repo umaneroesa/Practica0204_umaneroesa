b23d45c8f92ce992049cb2a8c02cafcdded00fc3
umaneroesa <umaneroesa1@educacion.navarra.es>
2024-10-29 11:31:56 +0100

    Fin10

---
+++ Ejercicio10.py
@@ -0,0 +1,6 @@
+#Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.
+precios = ["50","75", "46", "22", "80", "65", "08"]
+a = min(precios)
+print(a)
+b = max(precios)
+print("El menor precio es " + a + " y el mayor precio es "+ b)
